e1e24c5245719338b829d6109d03e0bc4cf479e2
Md Abdul Aziz <ratulrana88@gmail.com>
2026-06-26 19:19:25 -0500

    Update InversePINN_3K_TV.py

--- Inverse_PINN/3K/InversePINN_3K_TV.py
+++ Inverse_PINN/3K/InversePINN_3K_TV.py
@@ -181,8 +181,8 @@ def train_inverse_pinn():
     train_adam(model, optimizer, scheduler, epochs=8000, print_every=1000)
     print(f"\nPhase A completed in {time.time() - t0:.1f}s")
 
-    os.makedirs("ckpts_tv", exist_ok=True)
-    BEST_CKPT = "ckpts_tv/BEST_MODEL.pth"
+    os.makedirs("ckpts_3ktv", exist_ok=True)
+    BEST_CKPT = "ckpts_3ktv/BEST_MODEL.pth"
     best_loss = float("inf")
 
     get_lbfgs = lambda: optim.LBFGS(model.parameters(), lr=1.0, max_iter=100,
